c792d950c3d45ac764c2f7b9797d18eb937cb9b2
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-10-04 00:03:24 +0100

    a program that prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    print(''.join(chr(i).upper() if i % 2 != 0 else chr(i)), end='')
+    print('{}'.format(chr(i).upper() if i % 2 != 0 else chr(i), chr(i)), end='')
