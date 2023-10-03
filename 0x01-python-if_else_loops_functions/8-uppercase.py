0993eead0664ed2662669b6c829ef493ea3d79a3
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-10-03 15:23:12 +0100

    function to print uppercase strings

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for chars in str:
+        if 'a' <= chars <= 'z':
+            upper = chr(ord(chars) - ord('a') + ord('A'))
+        else:
+            upper = chars
+        print(upper, end="")
+    print()
