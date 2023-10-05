bf673638368a59127daa5bb37fed0200b3351803
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-10-05 13:53:12 +0100

    Command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,12 @@ if __name__ == "__main__":
     import sys
     arguments = sys.argv
     if len(arguments) == 1:
-        print("{} arguments".format(0))
+        print("0 arguments")
     elif len(arguments) == 2:
-        print("{} argument:".format(1))
-        for i, arg in enumerate(arguments[1:], 1):
-            print("{}: {}".format(i, arg))
+        print("1 argument:")
+        print("1: {}".format(arguments[1]))
     else:
-        print("{} arguments:".format(len(arguments)))
+        print("{} arguments:".format(len(arguments) - 1))
         for i, arg in enumerate(arguments[1:], 1):
             print("{}: {}".format(i, arg))
+
