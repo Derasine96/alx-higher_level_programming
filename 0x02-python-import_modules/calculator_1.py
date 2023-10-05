9dcbab249c3d0a0d98432537ef5bd47974a76e8e
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-10-05 13:09:25 +0100

    Python Modules

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -2,12 +2,18 @@
 def add(a, b):
     c = a + b
     return (c)
+
+
 def sub(a, b):
     c = a - b
     return (c)
+
+
 def mul(a, b):
     c = a * b
     return (c)
+
+
 def div(a, b):
     c = a / b
     return int((c))
