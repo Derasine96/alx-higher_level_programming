fd6abd76932ad2dad7b9b8ca011268b3ad3726d9
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-10-05 15:08:32 +0100

    Fix check error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+def print_arg(argv):
+    if len(argv) == 0:
+        print("0 arguments")
+    elif len(argv) == 1:
+        print("1 argument:")
+        for i, arg in enumerate(argv, 1):
+            print("{}: {}".format(i, arg))
+    else:
+        print("{} arguments:".format(len(argv)))
+        for i, arg in enumerate(argv, 1):
+            print("{}: {}".format(i, arg))
 if __name__ == "__main__":
     import sys
     arguments = sys.argv[1:]
-    if len(arguments) == 0:
-        print("0 arguments")
-    elif len(arguments) == 1:
-        print("1 argument:")
-        for i, arg in enumerate(arguments, 1):
-            print("{}: {}".format(i, arg))
-    else:
-        print("{} arguments:".format(len(arguments)))
-        for i, arg in enumerate(arguments, 1):
-            print("{}: {}".format(i, arg))
+    print_arg(arguments)
