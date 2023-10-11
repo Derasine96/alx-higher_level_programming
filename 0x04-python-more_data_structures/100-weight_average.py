b1e021c709743f07a3b882a3c18175ccd281f042
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-10-11 10:56:51 -0700

    average of tuples

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    total_score = 0
+    total_weight = 0
+    for score, weight in my_list:
+        total_score += score * weight
+        total_weight += weight
+    if total_weight == 0:
+        return 0
+    weighted_average = total_score / total_weight
+    return weighted_average
