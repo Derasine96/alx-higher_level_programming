812b1e2fa38a33495d59f36618e57a688ebe4cfa
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-10-23 05:45:24 -0700

    Fix: Task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,18 +2,20 @@
 def list_division(my_list_1, my_list_2, list_length):
     my_list = []
     for elements in range(list_length):
-        try:
-            if elements >= len(my_list_1) or elements >= len(my_list_2):
-                raise IndexError("out of range")
-            result = my_list_1[elements] / my_list_2[elements]
-        except ZeroDivisionError:
-            result = 0
-            print('division by 0')
-        except TypeError:
-            result = 0
-            print('wrong type')
-        except IndexError:
+        if elements >= len(my_list_1) or elements >= len(my_list_2):
             print('out of range')
-        finally:
-            my_list.append(result)
+            my_list.append(0)
+        else:
+            try:
+                result = my_list_1[elements] / my_list_2[elements]
+            except ZeroDivisionError:
+                result = 0
+                print('division by 0')
+            except TypeError:
+                result = 0
+                print('wrong type')
+            except IndexError:
+                print('out of range')
+            finally:
+                my_list.append(result)
     return my_list
