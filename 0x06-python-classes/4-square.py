42571615ca9569acb8841ba57cbbbe4ee8b98c97
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-10-24 11:33:31 -0700

    Fix: Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Represents a square"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialize a new Square.
 
         Args:
@@ -19,6 +19,11 @@ class Square:
 
     @size.setter
     def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The value of the new square size.
+        """
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
         elif value < 0:
