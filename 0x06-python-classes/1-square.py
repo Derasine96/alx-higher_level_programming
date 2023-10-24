0824c7c8b29fb437251cd98b0764af758a089c95
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-10-24 04:57:05 -0700

    Feature: Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size):
+        """Args:
+        param1: the self parameter
+        param2: size(int) The size of the new square"""
+    self.__size = size
