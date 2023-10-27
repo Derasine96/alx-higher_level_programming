17d18f895548c45f4cde07eb488f24cda2f0f708
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-10-27 10:13:41 -0700

    Fix: Task1 Feature: Task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,6 +5,7 @@ This module can be imported and used to perform division of a matrix,
 whether they are integers or floats.
 """
 
+
 def matrix_divided(matrix, div):
     """Divide elements of a matrix
 
@@ -20,14 +21,16 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ValueError("Divisor cannot be zero")
     row_size = len(matrix[0])
-    if not all(isinstance(row, list) and len(row) == row_size for row in matrix):
+    if not all(isinstance(row, list) and len(row) == row_size
+               for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     new_matrix = []
     for row in matrix:
         new_row = []
         for num in row:
             if not (isinstance(num, int) or isinstance(num, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            new_row.append(round(num / div, 2))
+                raise TypeError("matrix must be a matrix
+                                (list of lists) of integers/floats")
+                new_row.append(round(num / div, 2))
         new_matrix.append(new_row)
     return new_matrix
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Defines a function to print name(s)
+This module can be imported and used to print first and last name.
+Last name can be excluded when calling the function
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints a name or names
+
+    Args:
+        first_name (str): The first name (a string).
+        last_name (str, optional): string Defaults to "".
+    """
+    if not (isinstance(first_name, str) or isinstance(last_name, str)):
+        raise TypeError("first_name must be a string or
+                        last_name must be a string")
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    if not last_name:
+        print("Hello, my name is {}.".format(first_name))
+    else:
+        print("Hello, my name is {}, {}.".format(first_name, last_name))
