e78b8549153533a74b4b60b6bdb6a63f03ddbf41
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-10-28 11:55:48 -0700

    Feature: Task 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Define a function that prints a text
+with 2 new lines after each of these characters: ., ? and :
+This module can be imported and used to print a text
+The text is the string to be printed
+"""
+
+
+def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    char = 0
+    while char < len(text) and text[char] == ' ':
+        char += 1
+    while char < len(text):
+        print(text[char], end="")
+        if text[char] == "\n" or text[char] in ".?:":
+            if text[char] in ".?:":
+                print("\n")
+            char += 1
+            while char < len(text) and text[char] == ' ':
+                char += 1
+            continue
+        char += 1
