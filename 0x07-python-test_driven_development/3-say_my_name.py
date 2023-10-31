297b5ed7ccc0332b353e2362eeafae666957a847
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-10-31 15:36:00 -0700

    Feature: Task 6, 7

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -14,14 +14,11 @@ def say_my_name(first_name, last_name=""):
         first_name (str): The first name (a string).
         last_name (str, optional): string Defaults to "".
     """
-    if not (isinstance(first_name, str) or isinstance(last_name, str)):
-        raise TypeError("first_name must be a string or
-                        last_name must be a string")
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     if not last_name:
-        print("My name is {}.".format(first_name))
+        print("My name is {}".format(first_name))
     else:
-        print("My name is {} {}.".format(first_name, last_name))
+        print("My name is {} {}".format(first_name, last_name))
