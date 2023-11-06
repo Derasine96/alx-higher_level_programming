effb9b54a359d10de387c7bdc8960897f28cb1a9
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-11-06 05:58:50 -0800

    Tas4: issubclass instance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,6 +5,6 @@
 def inherits_from(obj, a_class):
     """Returns True if obj is an instance of a class
     that inherited from a_class, otherwise False."""
-    if issubclass(type(obj), a_class) and type(obj) == a_class:
-        return False
-    return True
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
