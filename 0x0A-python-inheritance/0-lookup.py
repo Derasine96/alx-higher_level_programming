7908d02458a8e2298914211aabf33d7474812305
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-11-06 02:55:51 -0800

    README and Task0: lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Returns list of available methods"""
+
+
+def lookup(obj):
+    """Returns a list
+    
+    Arg:
+        obj(object): object to be listed
+    """
+    return dir(obj)
