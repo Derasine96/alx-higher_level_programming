0960869cc32b0c9c8bafd3aacdc0357b86b86557
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-11-06 06:17:02 -0800

    Task6: Class exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define a Base Geometry."""
+
+
+class BaseGeometry:
+    def area(self):
+        """Raise an Exception with the message 'area() is not implemented'."""
+        raise Exception("area() is not implemented")
