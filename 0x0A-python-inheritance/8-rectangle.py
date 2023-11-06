958b52ca7fad49ca71750e3bfaa56bec6f51f3d6
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-11-06 08:07:10 -0800

    Instance of a subclass

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,26 +1,9 @@
 #!/usr/bin/python3
 """Define a Base Geometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry:
-    """Represent base geometry."""
-
-    def area(self):
-        """Raise an Exception with the message 'area() is not implemented'."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates a value
-
-        Args:
-            name (str): name (a string) for the value
-            value (int): value to validate
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-class Rectangle:
+class Rectangle(BaseGeometry):
     """Represents a Rectangle Subclass"""
 
     def __init__(self, width, height):
