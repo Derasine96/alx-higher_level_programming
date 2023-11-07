e3952883015d3b51a55ff35f52540e449af65c07
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-11-07 08:04:54 -0800

    Search and update a line in a file

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""Search and update a line in a text"""
+"""Defines a text file insertion function."""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """function that inserts a line of text to a file,
-        after each line containing a specific string
+    """Insert text after each line containing a given string in a file.
 
     Args:
-        search_string: checks for specific string
-        new_string: new string to insert
+        filename (str): The name of the file.
+        search_string (str): The string to search for within the file.
+        new_string (str): The string to insert.
     """
-    with open(filename. "r") as f:
-        strings = f.readline()
-    with open(filename, "w") as f:
-        for string in strings:
-            f.write(string)
+    strings = ""
+    with open(filename) as f:
+        for string in f:
+            strings += string
             if search_string in string:
-                f.write(new_string + '\n')
+                strings += new_string
+    with open(filename, "w") as file:
+        file.write(strings)
