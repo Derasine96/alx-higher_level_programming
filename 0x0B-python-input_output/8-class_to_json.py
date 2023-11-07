532919f7316a724eb7a04b4bfd8d7017d174d729
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-11-07 06:36:39 -0800

    Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Class to JSON"""
+
+
+def class_to_json(obj):
+    """A function that returns the dict. desc. with simple data structure
+        list, dict, str, int and bool for JSON serialization of an object
+
+    Arg:
+        obj: instance of a class
+    """
+    if not isinstance(obj, type):
+        serialized_dict = {}
+        for name, value in vars(obj).items():
+            if isinstance(value, (list, dict, str, int, bool)):
+                serialized_data[name] = value
+        return serialized_data
+    else:
+        raise ValueError("Input must be an instance of a class, not the class")
