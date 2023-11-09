8fb466fb9984a7e41275adcbacf6c4f56da0e800
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-11-09 09:07:59 -0800

    Adding a display function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,3 +73,8 @@ class Rectangle(Base):
     def area(self):
         """Return the current area of the rectangle."""
         return self.__width * self.__height
+
+    def display(self):
+        """Prints in stdout the Rectangle instance"""
+        for _ in range(self.__height):
+            print('#' * self.__width)
