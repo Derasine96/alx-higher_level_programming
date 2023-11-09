f0d7b0b0e6b4effbc580fb301487ff075366deba
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-11-09 08:39:30 -0800

    Raise errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,7 +7,7 @@ class Rectangle(Base):
     """A rectangle that inherits from Base with attributes."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """The rectangle class constructoor
+        """The rectangle class constructor
 
         Args:
             width: width of the rectangle
@@ -29,6 +29,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -38,6 +42,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -47,6 +55,8 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -56,4 +66,6 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
