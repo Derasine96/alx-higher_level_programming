4f0f7143af287800c84681183ba9334ea9cab570
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-11-10 07:41:45 -0800

    Square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A square is a rectangle with equal sides."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """The square class constructor
+
+        Args:
+            size (int): value of width and height(since it's a square)
+            x : x coordinate Defaults to 0.
+            y : y coordinate Defaults to 0.
+            id: Defaults to None.
+        """
+        super().__init__(size, size, x, y, id=id)
+
+    def area(self):
+        """Return the current area of the square."""
+        return self.width * self.width
+
+    def __str__(self):
+        """Return the str() representation of a Rectangle."""
+        return (f"[{self.__class__.__name__}] ({self.id}) "
+                f"{self.x}/{self.y} - {self.width}")
+
+        def update(self, *args, **kwargs):
+            """Assigns an argument to each attribute"""
+        if args:
+            if len(args) == 1:
+                self.id = args[0]
+            elif len(args) in {2, 3, 4}:
+                ar = [self.id, self.width, self.width, self.x, self.y]
+                for i in range(min(len(args), 5)):
+                    ar[i] = args[i]
+                self.id, self.width, self.width, self.x, self.y = ar
+            elif len(args) == 5:
+                self.id, self.width, self.width, self.x, self.y = args
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
