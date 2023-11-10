bdf1a9f1c058df0554f3c0687e920fd9b18db75b
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-11-10 08:47:53 -0800

    jSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,3 +18,16 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries
+
+        Args:
+            list_dictionaries (list): List of dictionaries.
+        """
+        import json
+
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
