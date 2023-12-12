d3d432d1e23fc1d76ce3ded49861bec4cf890c03
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-12-12 07:19:24 -0800

    Fix backticks instead of single quotes

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- Create a Table in MYSQL server
-CREATE TABLE IF NOT EXISTS 'first_table' (
-    'id' INT,
-    'name' VARCHAR(256)
+CREATE TABLE IF NOT EXISTS `first_table` (
+    `id` INT,
+    `name` VARCHAR(256)
 );
