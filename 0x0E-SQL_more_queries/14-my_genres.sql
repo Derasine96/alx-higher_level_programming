8853536f0763ac2e46f45cdc5d01ab9372ffbbf6
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-12-13 11:25:25 -0800

    SunQuery in MySQL server

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,8 @@
 -- A script that uses the hbtn_0d_tvshows database to list all genres of the show Dexter.
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+SELECT name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_show_genres.genre_id ASC;
+GROUP BY name
+ORDER BY name ASC;
