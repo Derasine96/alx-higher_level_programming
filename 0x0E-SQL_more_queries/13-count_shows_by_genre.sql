b78f35726966552a9cc7a1aa7c1cd320b73648c2
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-12-13 09:22:21 -0800

    Implementing RIGHT JOIN with COUNT

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,7 @@
--- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT  tv_show_genres, COUNT(tv_show_genres) AS genre, number_of_shows
-FROM tv_shows
-IINER JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
-ORDER BY number_of_shows ASC;
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+-- lists all rows of a database meeting a condition
+SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
+FROM tv_genres
+RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
