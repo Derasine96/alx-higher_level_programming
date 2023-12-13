b5ea48cd218c627bcce8ce05c73dcc4f83fc1d72
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-12-13 03:40:04 -0800

    A script to create a new User and Database and grant SELECT privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,7 @@
 -- Creates the database hbtn_0d_2 and the user user_0d_2
+-- Creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Creates a new user
 CREATE USER IF NOT EXISTS 'user'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Grants SELECT privileges
 GRANT SELECT ON hbtn_0d_2.* TO 'user'@'localhost'
