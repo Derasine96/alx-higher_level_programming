f479494d70e735630db680e5a6cb055dfe07b6f4
Derasine96 <chidexobikwelu@gmail.com>
2024-01-08 15:38:14 -0800

    a script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const num = process.argv;
+const square = parseInt(num[2]);
+
+if (isNaN(square)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < square; i++) {
+    let row = '';
+    for (let j = 0; j < square; j++) {
+      row += 'X';
+    }
+      console.log(row);
+  }
+}
