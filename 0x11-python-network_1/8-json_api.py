f3756f14b27660e1e1cf701f801359eded245958
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-01-26 07:37:17 -0800

    a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Python script that takes in a letter and sends a POST request"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    try:
+        response = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
