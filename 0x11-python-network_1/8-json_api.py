786aa6b14a7ade163cb59c4e59cc55080f784a1c
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-01-26 07:39:39 -0800

    a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,10 +10,12 @@ if __name__ == "__main__":
     else:
         q = ""
     try:
-        response = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+        response = requests.post('http://0.0.0.0:5000/search_user',
+                                 data={'q': q})
         json_response = response.json()
         if json_response:
-            print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
+            print("[{}] {}".format(json_response.get('id'),
+                                   json_response.get('name')))
         else:
             print("No result")
     except ValueError:
