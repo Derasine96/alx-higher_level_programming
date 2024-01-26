3a66604734cfb785a35e009800793b34a543eb21
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-01-26 06:07:56 -0800

    Add data encode ascii

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     email = sys.argv[2]
     values = {'email': email}
 
-    data = urllib.parse.urlencode(values)
+    data = urllib.parse.urlencode(values).encode("ascii")
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
